dec2f5174daa796832a40a3744434beb0b31badc
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-13 22:16:34 +0800

    [jianglin] fix: add cross origin in group api

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/GroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/GroupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/groups")
 public class GroupController {
     private final GroupService groupService;
